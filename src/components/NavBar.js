6efba2e763da7a1b210f4b375005c48113136134
joy Kurui <kurui.chepngetich@gmail.com>
2023-11-13 15:51:09 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElement = links.map((navLink) => {
+    return(
+     <a href={navLink} key={navLink}>{navLink}</a>
+   )});
+  return (
+  <nav>
+    {linkElement}
+  </nav>);
 }
 
 export default NavBar;
